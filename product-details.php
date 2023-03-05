171e13205d31d984862d1e8c78d066030f7c9776
Fadi <fadi@Fadis-MacBook-Pro.local>
2023-03-06 01:41:38 +0200

    add to product detail

---
+++ product-details.php
@@ -0,0 +1,27 @@
+<?php
+include('connectiob.php');
+
+$product_id = $_GET['product_id'];
+
+$query = $mysqli -> prepare('select name,image,description,price,quantity from products where id =?');
+$query -> bind_param('i',$product_id);
+$query -> execute();
+$query -> store_result();
+$num_rows = $query -> num_rows();
+$query -> bind_result($name,$image,$description,$price,$quantity);
+$query -> fetch();
+
+$response = [];
+
+if($num_rows == 0) {
+    $response['response'] = "Product not found";
+}
+else{
+    $response['name'] = $name;
+    $response['image'] = $image;
+    $response['description'] = $description;
+    $response['price'] = $price;
+    $response['quantity'] = $quantity;
+}
+
+echo json_encode($response);
