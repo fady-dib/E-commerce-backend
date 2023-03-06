b413237d5b9649bafec5829bd7ad666c3383369d
Fadi <fadi@Fadis-MacBook-Pro.local>
2023-03-07 00:48:31 +0200

    add cors header

--- products.php
+++ products.php
@@ -1,14 +1,16 @@
 <?php
+header("Access-Control-Allow-Origin: *");
+
 include('connection.php');
 
-$category_id = $_GET["category_id"];
-$query = $mysqli->prepare('select name,image,price from products where category_id=? ');
-$query->bind_param('i', $category_id);
+// $category_id = $_GET["category_id"];
+$query = $mysqli->prepare('select name,image,price,id from products');
+// $query->bind_param('i', $category_id);
 $query->execute();
 $query->store_result();
 $num_rows = $query->num_rows();
-$query->bind_result($name, $image, $price);
-$query->fetch();
+$query->bind_result($name, $image, $price,$id);
+// $query->fetch();
 $response = [];
 
 // echo $query;
@@ -19,7 +21,8 @@ if ($num_rows == 0) {
         $data = array(
         'name' => $name,
         'image' => $image,
-        'price' => $price
+        'price' => $price,
+        'id' => $id
         );
         array_push($response, $data);
     }
