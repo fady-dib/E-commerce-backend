afe21cadd48544ed1bf82a667ec3d253301eb2e1
Fadi <fadi@Fadis-MacBook-Pro.local>
2023-03-06 14:43:14 +0200

    while loop products

--- products.php
+++ products.php
@@ -1,23 +1,28 @@
 <?php
 include('connection.php');
 
-$category_id=$_GET["category_id"];
-$query = $mysqli ->prepare('select name,image,price from products where category_id=? ');
-$query -> bind_param('i',$category_id);
-$query-> execute();
-$query -> store_result();
-$num_rows=$query ->num_rows();
-$query->bind_result($name,$image,$price);
+$category_id = $_GET["category_id"];
+$query = $mysqli->prepare('select name,image,price from products where category_id=? ');
+$query->bind_param('i', $category_id);
+$query->execute();
+$query->store_result();
+$num_rows = $query->num_rows();
+$query->bind_result($name, $image, $price);
 $query->fetch();
-$response=[];
+$response = [];
 
-if($num_rows==0){
-    $response['response']="no products";
-}
-else{
-    $response['name']=$name;
-    $response['image']=$image;
-    $response['price']=$price;
+// echo $query;
+if ($num_rows == 0) {
+    $response['response'] = "no products";
+} else {
+    while ($query->fetch() && $num_rows !== 0)  {
+        $data = array(
+        'name' => $name,
+        'image' => $image,
+        'price' => $price
+        );
+        array_push($response, $data);
+    }
 }
 
 echo json_encode($response);
